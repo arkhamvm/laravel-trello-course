398adb56a04f1b9af736f2041a6fa82bde886e03
Vladimir Medvezhenkov <medvezhenkov.v@dns-shop.ru>
2021-08-22 23:21:01 +1000

    create card fix

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -9,7 +9,7 @@ class Card extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'desk_list_id'];
 
     public function tasks() {
         return $this->hasMany(Task::class);
